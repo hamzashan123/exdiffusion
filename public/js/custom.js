508655e41ec26fa678de7f2e003a7b69bafecd4e
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2023-08-16 22:05:04 +0500

    ll

--- public/js/custom.js
+++ public/js/custom.js
@@ -51,8 +51,8 @@ function getBaseModels(){
     // // Make an API request
     
     $.ajax({
-      url: '/get-base-models', // Replace with your API endpoint
-      //url: 'https://exdiffusion.com/newproject/public/get-base-models', // Replace with your API endpoint
+      //url: '/get-base-models', // Replace with your API endpoint
+      url: 'https://exdiffusion.com/newproject/public/get-base-models', // Replace with your API endpoint
       method: "GET",
       data: {
 
