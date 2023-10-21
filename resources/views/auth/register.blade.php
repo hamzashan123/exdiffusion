ec8d17fac6528267693ece5fa0ccd31f2f3d6bb0
Faraz Mundia <farazmuhammad1995@gmail.com>
2023-10-21 17:24:55 +0500

    updated form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Registration')
 @section('content')
 
-<div id="signupModal">
+<div id="signupModal" class="newSignUpForm">
     <div>
         <div class="modal-content">
 
@@ -31,14 +31,14 @@
                     <div class="form-group">
                         <label for="password">Password</label>
                         <div class="input-group">
-                        <input type="password" name="password" id="signupUserPassword" class="form-control">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button" id="togglePassword">
-                                <i class="fa fa-eye" id="eyeIcon"></i>
-                            </button>
+                            <input type="password" name="password" id="signupUserPassword" class="form-control">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                                    <i class="fa fa-eye" id="eyeIcon"></i>
+                                </button>
+                            </div>
                         </div>
-                        </div>
-                        
+
                         @error('password')<span class="text-danger" role="alert"><strong>{{ $message }}</strong></span>@enderror
                     </div>
 
@@ -46,14 +46,14 @@
                     <div class="form-group">
                         <label for="password_confirmation">Confirm Password</label>
                         <div class="input-group">
-                        <input type="password" name="password_confirmation" id="signupUserconfirmPassword" class="form-control">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button" id="togglePassword1">
-                                <i class="fa fa-eye" id="eyeIcon1"></i>
-                            </button>
+                            <input type="password" name="password_confirmation" id="signupUserconfirmPassword" class="form-control">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword1">
+                                    <i class="fa fa-eye" id="eyeIcon1"></i>
+                                </button>
+                            </div>
                         </div>
-                        </div>
-                        
+
                         @error('password_confirmation')<span class="text-danger" role="alert"><strong>{{ $message }}</strong></span>@enderror
                     </div>
 
@@ -79,33 +79,33 @@
 @endsection
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script>
-        $(document).ready(function () {
-            const passwordInput = $('#signupUserPassword');
-            const eyeIcon = $('#eyeIcon');
-            const togglePassword = $('#togglePassword');
+    $(document).ready(function() {
+        const passwordInput = $('#signupUserPassword');
+        const eyeIcon = $('#eyeIcon');
+        const togglePassword = $('#togglePassword');
 
-            togglePassword.click(function () {
-                if (passwordInput.attr('type') === 'password') {
-                    passwordInput.attr('type', 'text');
-                    eyeIcon.removeClass('fa-eye').addClass('fa-eye-slash');
-                } else {
-                    passwordInput.attr('type', 'password');
-                    eyeIcon.removeClass('fa-eye-slash').addClass('fa-eye');
-                }
-            });
-
-            const signupUserconfirmPassword = $('#signupUserconfirmPassword');
-            const eyeIcon1 = $('#eyeIcon1');
-            const togglePassword1 = $('#togglePassword1');
-
-            togglePassword1.click(function () {
-                if (signupUserconfirmPassword.attr('type') === 'password') {
-                    signupUserconfirmPassword.attr('type', 'text');
-                    eyeIcon1.removeClass('fa-eye').addClass('fa-eye-slash');
-                } else {
-                    signupUserconfirmPassword   .attr('type', 'password');
-                    eyeIcon1.removeClass('fa-eye-slash').addClass('fa-eye');
-                }
-            });
+        togglePassword.click(function() {
+            if (passwordInput.attr('type') === 'password') {
+                passwordInput.attr('type', 'text');
+                eyeIcon.removeClass('fa-eye').addClass('fa-eye-slash');
+            } else {
+                passwordInput.attr('type', 'password');
+                eyeIcon.removeClass('fa-eye-slash').addClass('fa-eye');
+            }
         });
+
+        const signupUserconfirmPassword = $('#signupUserconfirmPassword');
+        const eyeIcon1 = $('#eyeIcon1');
+        const togglePassword1 = $('#togglePassword1');
+
+        togglePassword1.click(function() {
+            if (signupUserconfirmPassword.attr('type') === 'password') {
+                signupUserconfirmPassword.attr('type', 'text');
+                eyeIcon1.removeClass('fa-eye').addClass('fa-eye-slash');
+            } else {
+                signupUserconfirmPassword.attr('type', 'password');
+                eyeIcon1.removeClass('fa-eye-slash').addClass('fa-eye');
+            }
+        });
+    });
 </script>
