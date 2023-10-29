c7a9b3731963d1a8d163f32fc32abffc00af7126
Faraz Mundia <farazmuhammad1995@gmail.com>
2023-10-29 19:06:37 +0500

    changes

--- resources/views/frontend/exdiffusion/publishedCreation.blade.php
+++ resources/views/frontend/exdiffusion/publishedCreation.blade.php
@@ -34,12 +34,12 @@
     <div class="row" id="publicCreationImagesList">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="masonry">
-                
+
             </div>
         </div>
     </div>
     <div class="row" id="publicCreationModelList">
-        
+
     </div>
 </div>
 @endsection('content')
@@ -47,107 +47,107 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
         var publishCreationArray = [];
 
-        function getPublishCreations(modelType){
-                $('#publicCreationModelList').hide();
-                $('#publicCreationImagesList').show();
+        function getPublishCreations(modelType) {
+            $('#publicCreationModelList').hide();
+            $('#publicCreationImagesList').show();
 
-                $.ajax({
-                    url: "" + baseUrl + "/get-publish-creation",
-                    method: "POST",
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    data: {
-                        modelType : modelType
-                    },
-                    success: function (response) {
-                        console.log(response);
+            $.ajax({
+                url: "" + baseUrl + "/get-publish-creation",
+                method: "POST",
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {
+                    modelType: modelType
+                },
+                success: function(response) {
+                    console.log(response);
 
-                        $(".masonry").empty();
+                    $(".masonry").empty();
 
-                        if(response.data.length > 0){
-                            response.data.forEach((element) => {
-                            console.log("image_url",element.image_url);
+                    if (response.data.length > 0) {
+                        response.data.forEach((element) => {
+                            console.log("image_url", element.image_url);
                             var pageHTML = "<div class='grid'>";
-                                pageHTML += "<img src='"+element.image_url+"'>";
-                                pageHTML += "<div class='grid__body'>";
-                                pageHTML += "<div class='relative'>";
-                                pageHTML += "<a class='grid__link' href=''></a>";
-                                pageHTML += "<input type='checkbox' name='' data-creativeId='"+element.id+"' class='imageCheckCreativehistory'>";
-                                
-                                pageHTML += "</div>";
-                                pageHTML += "<div class='mt-auto masonry-btn-generate'>";
-                                pageHTML += "<button class='btn purple-col-bg form-control text-white border-radius-7 generateCreativeHistory' data-creativeId='"+element.id+"'>Generate</button>";
-                                pageHTML += "</div>";
-                                pageHTML += "</div>";
-                                pageHTML += "</div>";
-                            $(".masonry").append(pageHTML);
-                            });
-                            
-                        
-                            $("#loader").hide();
-                        }else{
-                            var pageHTML = "<div class='grid'>";
-                            pageHTML += "<p class='text-white'> No images found in favorites!</p>";
+                            pageHTML += "<img src='" + element.image_url + "'>";
+                            pageHTML += "<div class='grid__body'>";
+                            pageHTML += "<div class='relative'>";
+                            pageHTML += "<a class='grid__link' href=''></a>";
+                            pageHTML += "<input type='checkbox' name='' data-creativeId='" + element.id + "' class='imageCheckCreativehistory'>";
+
                             pageHTML += "</div>";
-
+                            pageHTML += "<div class='mt-auto masonry-btn-generate'>";
+                            pageHTML += "<button class='btn purple-col-bg form-control text-white border-radius-7 generateCreativeHistory' data-creativeId='" + element.id + "'>Generate</button>";
+                            pageHTML += "</div>";
+                            pageHTML += "</div>";
+                            pageHTML += "</div>";
                             $(".masonry").append(pageHTML);
-                        
-                            
-                            $("#loader").hide();
-                        }
-                    
-                        
-                    },
-                    error: function () {
-                        $("#result").text(
-                            "Error occurred while fetching data from the API."
-                        );
-                    },
-                });
-            
+                        });
+
+
+                        $("#loader").hide();
+                    } else {
+                        var pageHTML = "<div class='grid'>";
+                        pageHTML += "<p class='text-white'> No images found in favorites!</p>";
+                        pageHTML += "</div>";
+
+                        $(".masonry").append(pageHTML);
+
+
+                        $("#loader").hide();
+                    }
+
+
+                },
+                error: function() {
+                    $("#result").text(
+                        "Error occurred while fetching data from the API."
+                    );
+                },
+            });
+
         }
 
-        function getAllModels(modelType){
-                $('#publicCreationImagesList').hide();
-                $('#publicCreationModelList').show();
-                
-                $.ajax({
-                
+        function getAllModels(modelType) {
+            $('#publicCreationImagesList').hide();
+            $('#publicCreationModelList').show();
+
+            $.ajax({
+
                 url: "" + baseUrl + "/get-base-models", // Replace with your API endpoint
                 method: "GET",
                 data: {},
-                success: function (response) {
+                success: function(response) {
                     var response = JSON.parse(response);
 
                     console.log("response", response);
 
                     if (response.status == "success") {
-                    
-                        $("#publicCreationModelList").empty();
-                        if(modelType == 'base_models'){
-                            response.models.forEach((element) => {
-                            var pageHTML =
-                                "<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>";
-                            pageHTML += "<div class='bodyInner'>";
-                            pageHTML +=
-                                "<img src='" +
-                                baseUrl +
-                                "/img/icons/placeholder.png' alt='Image 1' class='img-fluid mb-3'>";
-                            pageHTML += " </div>";
-                            pageHTML += " <span> " + element.model_id + "</span>";
-                            pageHTML += "</div>";
 
-                            $("#publicCreationModelList").append(pageHTML);
-                        });
+                        $("#publicCreationModelList").empty();
+                        if (modelType == 'base_models') {
+                            response.models.forEach((element) => {
+                                var pageHTML =
+                                    "<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>";
+                                pageHTML += "<div class='bodyInner'>";
+                                pageHTML +=
+                                    "<img src='" +
+                                    baseUrl +
+                                    "/img/icons/placeholder.png' alt='Image 1' class='img-fluid mb-3'>";
+                                pageHTML += " <span> " + element.model_id + "</span>";
+                                pageHTML += " </div>";
+                                pageHTML += "</div>";
+
+                                $("#publicCreationModelList").append(pageHTML);
+                            });
                         }
 
-                        
-                        if(modelType == 'lora_models'){
+
+                        if (modelType == 'lora_models') {
                             if (response.lora_models[0] !== undefined) {
                                 response.lora_models.forEach((element) => {
                                     var pageHTML =
@@ -157,16 +157,16 @@
                                         "<img src='" +
                                         baseUrl +
                                         "/img/icons/placeholder.png' alt='Image 1' class='img-fluid mb-3'>";
-                                    pageHTML += " </div>";
                                     pageHTML += " <span> " + element.model_id + "</span>";
+                                    pageHTML += " </div>";
                                     pageHTML += "</div>";
 
                                     $("#publicCreationModelList").append(pageHTML);
                                 });
                             }
                         }
-                       
-                        if(modelType == 'embedding_models'){
+
+                        if (modelType == 'embedding_models') {
                             if (response.embeddings_models[0] !== undefined) {
                                 response.embeddings_models.forEach((element) => {
                                     var pageHTML =
@@ -176,8 +176,8 @@
                                         "<img src='" +
                                         baseUrl +
                                         "/img/icons/placeholder.png' alt='Image 1' class='img-fluid mb-3'>";
-                                    pageHTML += " </div>";
                                     pageHTML += " <span> " + element.model_id + "</span>";
+                                    pageHTML += " </div>";
                                     pageHTML += "</div>";
 
                                     $("#publicCreationModelList").append(pageHTML);
@@ -188,7 +188,7 @@
                         $("#loader").hide();
                     }
                 },
-                error: function () {
+                error: function() {
                     $("#result").text(
                         "Error occurred while fetching data from the API."
                     );
@@ -196,13 +196,13 @@
             });
         }
 
-        $(document).on('change','.imageCheckCreativehistory', function() {
+        $(document).on('change', '.imageCheckCreativehistory', function() {
             var checkedCheckboxes = $('.imageCheckCreativehistory:checked').length;
             var creativeId = $(this).data("creativeid");
-        
-            if(!publishCreationArray.includes(creativeId)){
+
+            if (!publishCreationArray.includes(creativeId)) {
                 publishCreationArray.push(creativeId);
-            }else{
+            } else {
                 const index = publishCreationArray.indexOf(creativeId);
                 if (index > -1) {
                     publishCreationArray.splice(index, 1);
@@ -221,37 +221,37 @@
 
         getPublishCreations();
 
-        $(document).on('click','#publishcreation_images_filter', function() { 
+        $(document).on('click', '#publishcreation_images_filter', function() {
             $("#loader").show();
             getPublishCreations("Images");
         });
 
-        $(document).on('click','#publishcreation_favourite_filter', function() { 
+        $(document).on('click', '#publishcreation_favourite_filter', function() {
             $("#loader").show();
             getPublishCreations("Favourite");
         });
 
-        $(document).on('click','#publishcreation_basemodel_filter', function() { 
+        $(document).on('click', '#publishcreation_basemodel_filter', function() {
             $("#loader").show();
             getAllModels('base_models');
         });
 
-        $(document).on('click','#publishcreation_lora_filter', function() { 
+        $(document).on('click', '#publishcreation_lora_filter', function() {
             $("#loader").show();
             getAllModels('lora_models');
         });
 
-        $(document).on('click','#publishcreation_embedding_filter', function() { 
+        $(document).on('click', '#publishcreation_embedding_filter', function() {
             $("#loader").show();
             getAllModels('embedding_models');
         });
 
-        
-        $(document).on('click','#public_creation_apply_filters', function() {
+
+        $(document).on('click', '#public_creation_apply_filters', function() {
             console.log(publishCreationArray);
             var selectedAction = $('#public_creation_filterlist').val();
 
-            if(selectedAction == 'addToFavoritePublishCreation'){
+            if (selectedAction == 'addToFavoritePublishCreation') {
                 $("#apply_filters").find('.loaderbtn').show();
                 $("#loader").show();
                 $.ajax({
@@ -262,9 +262,9 @@
                     },
                     data: {
                         creativeArray: publishCreationArray,
-                        publishcreation  : true
+                        publishcreation: true
                     },
-                    success: function (response) {
+                    success: function(response) {
                         $("#apply_filters").find('.loaderbtn').hide();
                         $("#loader").hide();
                         console.log(response);
@@ -275,9 +275,9 @@
                             showConfirmButton: false
                         });
                         //call getUserCreative history function to reload images
-                        getPublishCreations();  
+                        getPublishCreations();
                     },
-                    error: function () {
+                    error: function() {
                         $("#apply_filters").find('.loaderbtn').hide();
                         $("#loader").hide();
                         $("#result").text(
@@ -285,13 +285,13 @@
                         );
                     },
                 });
-            }else{
+            } else {
                 alert('Select an option to apply!');
             }
 
         });
 
-        $(document).on('click','.generateCreativeHistory', function() {
+        $(document).on('click', '.generateCreativeHistory', function() {
 
             var creativeId = $(this).data("creativeid");
             $("#loader").show();
@@ -302,29 +302,29 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
                 data: {
-                    creativeId : creativeId
+                    creativeId: creativeId
                 },
-                success: function (response) {
+                success: function(response) {
                     console.log(response);
-                    if(response.status == 'success'){
+                    if (response.status == 'success') {
                         console.log(response.data);
                         $("#loader").hide();
                         localStorage.removeItem("creativeData");
-                        localStorage.setItem("creativeData",JSON.stringify(response.data));
+                        localStorage.setItem("creativeData", JSON.stringify(response.data));
 
                         localStorage.removeItem("globalLoraModelArray");
-                        localStorage.setItem("globalLoraModelArray",loraModelArray);
-                        
+                        localStorage.setItem("globalLoraModelArray", loraModelArray);
+
                         // window.location.href = 'newproject/public/playground?generated=true';
-                        window.location.href = baseUrl+'/playground?generated=true';
-                    
-                    }else{
+                        window.location.href = baseUrl + '/playground?generated=true';
+
+                    } else {
                         console.log(response);
                         $("#loader").hide();
                     }
-                    
+
                 },
-                error: function () {
+                error: function() {
                     $("#loader").hide();
                     $("#result").text(
                         "Error occurred while fetching data from the API."
@@ -332,7 +332,7 @@
                 },
             });
 
-            });
-
         });
+
+    });
 </script>
