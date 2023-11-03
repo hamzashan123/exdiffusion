992283370e5ae49cfb5b18bef1e8e4069cbe9270
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2023-11-03 18:54:56 +0500

    asd

--- app/Http/Controllers/Frontend/ModelsController.php
+++ app/Http/Controllers/Frontend/ModelsController.php
@@ -298,7 +298,7 @@ class ModelsController extends Controller
               Storage::disk($storagePath)->put($filename, $imgResponse->getBody());
               // dd($filename);
 
-              if(!empty($request->creativeHistoryId) || $request->creativeHistoryId != null){
+              if(!empty($request->creativeHistoryId) || $request->creativeHistoryId != 'undefined'){
                 // dd($request->creativeHistoryId);
                 $creativeData = DB::table('creativehistory')->where('id',$request->creativeHistoryId)->first();
                   $Id  = DB::table('creativehistory')->insertGetId([
