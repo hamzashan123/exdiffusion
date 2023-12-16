9a95844bc007f0e5485e344b4d71145a4c961f64
Faraz Mundia <farazmuhammad1995@gmail.com>
2023-12-16 18:47:52 +0500

    hh

--- resources/views/frontend/exdiffusion/publishedCreation.blade.php
+++ resources/views/frontend/exdiffusion/publishedCreation.blade.php
@@ -77,11 +77,14 @@
                     $(".masonry").empty();
 
                     if (modelType == "NSFW") {
-                        var pageHTML = "<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>";
+                        $('#blurringContainer').remove();
+                        var pageHTML = "<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12' id='blurringContainer'>";
                         pageHTML += "<a class='showNSFW'> <img src='https://exdiffusion.com/newproject/public/img/icons/eye-cut.png'/> </a> <span class='blurringText'>  blurring is <span class='updateBlueText'> on </span> </span>";
                         pageHTML += "</div>";
                         $("#publicCreationImagesList").prepend(pageHTML);
                         console.log('modelType New', modelType);
+                    } else {
+                        $('#blurringContainer').remove();
                     }
 
                     if (response.data.length > 0) {
