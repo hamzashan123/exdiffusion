7096ab0d77652abfe44969f00c9b0e85fcea4fe5
Faraz Mundia <farazmuhammad1995@gmail.com>
2023-12-29 19:09:59 +0500

    asda

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,29 +9,30 @@
     <meta name="description" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
-  <link rel="icon" href="{{ asset('img/icons/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('img/icons/favicon.ico') }}" type="image/x-icon">
     <title>{{ config('app.name', 'Laravel') }} | @yield('title', 'Dashboard')</title>
     <!-- Fonts -->
     <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet"> -->
     <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-   
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/css/sb-admin-2.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('backend/vendor/bootstrap-fileinput/css/fileinput.min.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/vendor/summernote/summernote-bs4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/css/custom.css') }}">
-    <livewire:styles/>
+    <livewire:styles />
     @yield('styles')
 </head>
+
 <body id="page-top">
 
     <div id="app">
         <div id="wrapper">
             @include('partials.backend.sidebar')
-            <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content-wrapper" class="d-flex flex-column mainRightContainer">
                 <div id="content">
                     @include('partials.backend.navbar')
                     <!-- Begin Page Content -->
@@ -49,12 +51,12 @@
 
         @include('partials.backend.modal')
     </div>
-    <livewire:scripts/>
+    <livewire:scripts />
     <script src="{{ asset('js/app.js') }}"></script>
-    
+
     <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
-    
+
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
     <script src="{{ asset('backend/js/custom.js') }}"></script>
@@ -65,7 +67,8 @@
     <script src="{{ asset('backend/vendor/bootstrap-fileinput/themes/fas/theme.min.js') }}"></script>
     <!-- summernote -->
     <script src="{{ asset('backend/vendor/summernote/summernote-bs4.min.js') }}"></script>
-    
+
     @yield('scripts')
 </body>
+
 </html>
--- resources/views/partials/backend/footer.blade.php
+++ resources/views/partials/backend/footer.blade.php
@@ -1,10 +1,11 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <!-- <span>Copyright &copy; farazmundia</span> -->
+            <span>Copyright &copy; </span>
             <script>
                 document.write(new Date().getFullYear())
             </script>
+            Exdiffusion Admin Panel
         </div>
     </div>
 </footer>
